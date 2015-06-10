1d39b84710c7ab33b504a792c733631e36e26c5a
foobert <0x002a@gmail.com>
2015-06-10 07:09:24 +0200

    Hack in experimental download

--- web2/js/index.js
+++ web2/js/index.js
@@ -1,4 +1,32 @@
 $(function() {
     $('.ui.checkbox').checkbox();
     $('.ui.radio.checkbox').checkbox();
+
+    $('.submit.button').click(function() {
+        $('.form').addClass('loading');
+        var files = [
+            {url: 'https://gc.funkenburg.net/api/poi.csv?type=3', name: 'multi.csv'},
+            {url: 'http://gc.funkenburg.net/api/poi.csv?type=2', name: 'tradi.csv'}
+        ];
+        var zip = new JSZip();
+        var zipFolder = zip.folder('poi');
+        var h = function(name, data) {
+            if (data != null) {
+                zipFolder.file(name, data);
+            }
+            if (files.length === 0) {
+                var zipBlob = zip.generate({type: 'blob'});
+                saveAs(zipBlob, 'poi.zip');
+                $('.form').removeClass('loading');
+            } else {
+                var next = files.shift();
+                $.get(next.url)
+                    .done(function(data) { h(next.name, data); })
+                    .fail(function() {
+                        $('.form').removeClass('loading').addClass('error');
+                    });
+            }
+        };
+        h();
+    });
 });
