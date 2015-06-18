a31026ec1eb5f8f330fe02461d57a1a6cca9f7e1
foobert <0x002a@gmail.com>
2015-06-18 20:17:24 +0200

    auto prefix to give other browsers some love

--- web2/webpack.config.js
+++ web2/webpack.config.js
@@ -1,3 +1,5 @@
+var autoprefixer = require('autoprefixer-core');
+
 module.exports = {
     entry: "./src/index.coffee",
     output: {
@@ -8,11 +10,12 @@ module.exports = {
     devtool: "source-map",
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" },
+            { test: /\.css$/, loader: "style!css!postcss" },
             { test: /\.coffee$/, loader: "coffee-loader" },
             { test: /\.cjsx$/, loaders: ['coffee', 'cjsx']},
             { test: /\.(jpe?g|png|gif|svg)$/i, loaders: [ 'file?hash=sha512&digest=hex&name=[hash].[ext]' ] },
             { test: /\.(woff2?|ttf|eot)$/i, loaders: [ 'file?hash=sha512&digest=hex&name=[hash].[ext]' ] }
         ]
-    }
+    },
+    postcss: [ autoprefixer({ browsers: ['last 2 version'] }) ]
 };
