1ac4c884c69c614adf0d773a1f00f6c6b41c8b61
foobert <0x002a@gmail.com>
2015-06-19 19:35:49 +0200

    Add some logging to server.js

--- web2/server.js
+++ web2/server.js
@@ -1,8 +1,11 @@
 var compression = require('compression');
 var express = require('express');
+var logger = require('morgan');
 var app = express();
 app.set('x-powered-by', false);
 app.use(compression());
+app.use(logger(process.env.NODE_ENV === 'production' ? 'tiny' : 'dev'));
 app.use('/assets', express.static('assets'));
 app.get('/*', function(req, res, next) { res.sendFile('index.html', {root: __dirname}); });
+console.log('Listening on 8080');
 app.listen(8080);
