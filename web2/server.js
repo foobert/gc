6eb0de27e61baa7f77a37eedb2dfbfc3da7ce0c9
foobert <0x002a@gmail.com>
2015-06-19 19:23:12 +0200

    Try to optimize size for prod

--- web2/server.js
+++ web2/server.js
@@ -1,5 +1,8 @@
+var compression = require('compression');
 var express = require('express');
 var app = express();
+app.set('x-powered-by', false);
+app.use(compression());
 app.use('/assets', express.static('assets'));
 app.use('/img', express.static('img'));
 app.get('/*', function(req, res, next) { res.sendFile('index.html', {root: __dirname}); });
