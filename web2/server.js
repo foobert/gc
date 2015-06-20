ced6431e3f5eae451e14a5040b6fed2fd8653362
foobert <0x002a@gmail.com>
2015-06-20 08:14:50 +0200

    Move favicon
    So that it is picked up by webpack dev server as well.

--- web2/server.js
+++ web2/server.js
@@ -6,7 +6,7 @@ app.set('x-powered-by', false);
 app.use(compression());
 app.use(logger(process.env.NODE_ENV === 'production' ? 'tiny' : 'dev'));
 app.use('/assets', express.static('assets'));
-app.get('/favicon.png', function(req, res, next) { res.sendFile('img/favicon.png', {root: __dirname}); });
+app.get('/favicon.png', function(req, res, next) { res.sendFile('favicon.png', {root: __dirname}); });
 app.get('/*', function(req, res, next) { res.sendFile('index.html', {root: __dirname}); });
 console.log('Listening on 8080');
 app.listen(8080);
