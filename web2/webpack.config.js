090dfe05800d803791f8026696464194d60fdb97
foobert <0x002a@gmail.com>
2015-06-26 20:43:00 +0200

    Inline small images

--- web2/webpack.config.js
+++ web2/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
             { test: /\.css$/, loader: "style!css" },
             { test: /\.coffee$/, loader: "coffee-loader" },
             { test: /\.cjsx$/, loaders: ['coffee', 'cjsx']},
-            { test: /\.(jpe?g|png|gif|svg)$/i, loaders: [ 'file?hash=sha512&digest=hex&name=[hash].[ext]' ] },
+            { test: /\.(jpe?g|png|gif|svg)$/i, loaders: [ 'url?limit=2048!file?hash=sha512&digest=hex&name=[hash].[ext]' ] },
             { test: /\.(woff2?|ttf|eot)$/i, loaders: [ 'file?hash=sha512&digest=hex&name=[hash].[ext]' ] }
         ]
     },
