a8b854751643e1610225a1ce9f8e1af90a406621
foobert <0x002a@gmail.com>
2015-06-30 19:09:48 +0200

    Just use the autoprefixer for all css files

--- web2/webpack.config.js
+++ web2/webpack.config.js
@@ -10,8 +10,7 @@ module.exports = {
     devtool: "cheap-module-eval-source-map",
     module: {
         loaders: [
-            { test: /^css\/\.css$/, loader: "style!css!postcss" },
-            { test: /\.css$/, loader: "style!css" },
+            { test: /\.css$/, loader: "style!css!postcss" },
             { test: /\.coffee$/, loader: "coffee-loader" },
             { test: /\.cjsx$/, loaders: ['coffee', 'cjsx']},
             { test: /\.(jpe?g|png|gif|svg)$/i, loaders: [ 'url?limit=2048!file?hash=sha512&digest=hex&name=[hash].[ext]' ] },
