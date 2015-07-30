b845f1422726a69aee6ecc4bebb433d964ce28f9
foobert <0x002a@gmail.com>
2015-07-30 20:37:19 +0200

    Add hot-reloading dev mode

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,10 +1,16 @@
+var webpack = require('webpack');
 var autoprefixer = require('autoprefixer-core');
+var path = require('path');
 
 module.exports = {
-    entry: "./src/index.coffee",
+    entry: [
+        'webpack-dev-server/client?http://0.0.0.0:9090',
+        'webpack/hot/only-dev-server',
+        './src/index.coffee'
+    ],
     output: {
-        path: './assets',
-        publicPath: "assets/",
+        path: path.join(__dirname, 'assets'),
+        publicPath: "/assets/",
         filename: "bundle.js"
     },
     devtool: "cheap-module-eval-source-map",
@@ -13,10 +19,14 @@ module.exports = {
             { test: /\.css$/, loaders: ['style', 'css', 'postcss'] },
             { test: /\.coffee$/, loaders: ['coffee'] },
             { test: /\.js$/, exclude: /node_modules/, loaders: ['babel?optional[]=runtime&stage=1'] },
-            { test: /\.cjsx$/, loaders: ['coffee', 'cjsx']},
+            { test: /\.cjsx$/, loaders: ['react-hot', 'coffee', 'cjsx']},
             { test: /\.(jpe?g|png|gif|svg)$/i, loaders: [ 'url?limit=2048!file?hash=sha512&digest=hex&name=[hash].[ext]' ] },
             { test: /\.(woff2?|ttf|eot)$/i, loaders: [ 'file?hash=sha512&digest=hex&name=[hash].[ext]' ] }
         ]
     },
-    postcss: [ autoprefixer({ browsers: ['last 2 version'] }) ]
+    postcss: [ autoprefixer({ browsers: ['last 2 version'] }) ],
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ]
 };
