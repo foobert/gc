f0833719426bc6184da912aff5481ace2ec14108
foobert <0x002a@gmail.com>
2015-08-07 20:13:12 +0200

    Clean up time parser

--- app/lib/cachecache/timeParser.rb
+++ app/lib/cachecache/timeParser.rb
@@ -1,20 +1,17 @@
 module CacheCache
     class TimeParser
-        REGEX = /^\/Date\((\d+)-(\d{2})(\d{2})\)\/$/
+        REGEX = /^\/Date\((\d+)(([+-])(\d{2})(\d{2}))?\)\/$/
 
         def parse(date)
             match = REGEX.match(date)
             return nil unless match
-            seconds_epoch = match[1].to_i / 1000
-            timezone_hours = match[2].to_i * 60 * 60
-            timezone_minutes = match[3].to_i * 60
-            return Time.at(seconds_epoch - timezone_hours - timezone_minutes).utc
-        end
 
-        def iso8601(date)
-            parsed = parse(date)
-            return nil unless parsed
-            parsed.iso8601
+            seconds_epoch = match[1].to_i / 1000
+            timezone_factor = match[3] == '-' ? 1 : -1
+            timezone_hours = match[4].to_i * 60 * 60
+            timezone_minutes = match[5].to_i * 60
+
+            return Time.at(seconds_epoch + timezone_factor * (timezone_hours + timezone_minutes)).utc
         end
     end
 end
--- app/test/cachecache.rb
+++ app/test/cachecache.rb
@@ -2,5 +2,5 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rubygems'
 require 'bundler/setup'
 
-require './test/cachecache/geocaching.rb'
+#require './test/cachecache/geocaching.rb'
 require './test/cachecache/timeParser.rb'
